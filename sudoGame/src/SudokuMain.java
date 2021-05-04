a90a62af935adea8ab832a79a8b767ee1ba12df8
stormsidali2001 <53428196+stormsidali2001@users.noreply.github.com>
2021-05-04 14:26:24 +0500

    Update SudokuMain.java

--- sudoGame/src/SudokuMain.java
+++ sudoGame/src/SudokuMain.java
@@ -1,6 +1,10 @@
-public class SodukoMain {
+public class SodokuMain {
     public static void main (String[] args){
         SudokuPanel panel = new   SudokuPanel();
-    
+        panel.setText(0, 1, "9");
+        Sudoku s = new Sudoku();
+        panel.setS(s);
+       
+       
     }
 }
