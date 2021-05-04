48413eb7c9a203119c7d881d61dbb75ea9ce93b6
stormsidali2001 <53428196+stormsidali2001@users.noreply.github.com>
2021-05-04 15:06:35 +0500

    Update Sudoku.java

--- sudoGame/src/Sudoku.java
+++ sudoGame/src/Sudoku.java
@@ -1,136 +1,251 @@
-import java.awt.*;
-import java.awt.event.*;
+import java.util.HashSet;
 import java.util.*;
-import javax.swing.*;
 
-public class SudokuPanel implements ActionListener {
-    public static JFrame f = new JFrame("Sudoku");
-    public static JPanel p = new JPanel();
-    public static JTextField[][]M = new JTextField[9][9];
-    public static JTextField[][] CopieM = new JTextField[9][9];
-    private Sudoku s;
-
-    public Sudoku getS() {
-        return this.s;
+public class Sudoku{
+    private char[][] board;
+    public String tmp="";
+    public char[][] getSudoku() {
+        return this.board;
     }
 
-    public void setS(Sudoku s) {
-        this.s = s;
+    public void setSudoku(char[][] board) {
+        this.board = board;
+    }
+    public void setVal(int i,int j,char val)
+    {
+        this.board[i][j] = val;
+    }
+    public Sudoku()
+    {
+       init();
+    }
+    public void init()
+    {
+        board=new char[9][9];
         for(int i=0;i<9;i++)
         {
+           
             for(int j=0;j<9;j++)
             {
-                
-               setText(i, j, String.valueOf(s.getSudoku()[i][j]));
+                board[i][j]='.';
             }
         }
     }
-
-    public Font getFont() {
-        return this.font;
-    }
-
-    public void setFont(Font font) {
-        this.font = font;
-    }
-    Font font = new Font("Lucida Console", Font.BOLD,28);
-
-   
-    SudokuPanel(){
-        f.setSize(600,600);
-        p.setVisible(false);
-        p.setLayout(new GridLayout(9,9));
-        for (int i = 0; i<9;i++){
-            for (int j = 0; j<9;j++){
-                M[i][j]= new JTextField();
-                M[i][j].setFont(font);
-                M[i][j].setForeground(Color.black);
-                M[i][j].setHorizontalAlignment(SwingConstants.CENTER);
-                M[i][j].addActionListener(this);
-                p.add(M[i][j]);
-                
-               
-            }
-        }
-        f.setContentPane(p);
-        p.setVisible(true);
-        f.setVisible(true);
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.setResizable(false);
-        int x,y;
-        for (x = 0;x<9;x++){
-            for (y = 0;y<9;y++){
-                switch(x){
-                    case 0:case 1:case 2:case 6: case 7: case 8:{
-                         switch(y){
-                            case 0:case 1:case 2: case 6:case 7:case 8:{
-                                M[x][y].setBackground(Color.BLUE);
-                            }                            
-                        }
-                    } 
-                }
-            }
-        }
+    public boolean validInColumn(int r,int c)
+    {
+        
+        for(int i=0;i<9;i++ )
+        {
+            if(i==r) continue;
+           
+            if(board[i][c]==board[r][c]) return false;
             
-        for (x = 0;x<9;x++){
-            for (y = 0;y<9;y++){
-                switch(x){
-                    case 3:case 4:case 5:{
-                          switch (y){             
-                             case 3:case 4:case 5:{
-                               M[x][y].setBackground(Color.BLUE);
-                 
-                            }                           
-                        }  
-                   }
-                }  
-            }
+        }
+        return true;
+    }
+    public boolean validInRow(int r,int c)
+    {
+        
+        for(int j=0;j<9;j++ )
+        {
+            
+            if( j==c) continue;
+          
+            if(board[r][j]==board[r][c]) return false;
+            
+        }
+        return true;
+    }
+    public void  generateSudo(int numGaps)
+    {
+        this.solve(0, 0);
+        Random random = new Random();
+        for(int i=0;i<numGaps;i++)
+        {
+           board[random.nextInt(9)][random.nextInt(9)] ='.';
         }
     }
-    public void setText(int i , int j , String k  ){
-        
-      
-       M[i][j].setText(k);
-        
-    }
-    
-   
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        // TODO Auto-generated method stub
-        for(int i=0;i<9;i++)
+    public boolean validInBox(int r,int c)
+    {
+
+        int ibox=((int)(r/3))*3;
+        int jbox=((int)(c/3))*3;
+        for(int i=0;i<3;i++)
         {
-            for(int j=0;j<9;j++)
+            for(int j=0;j<3;j++)
             {
-                if(e.getSource()==M[i][j])
+                
+               
+                 if(((ibox+i==r) && (j+jbox==c))|| board[i+ibox][j+jbox]=='.') continue;
+               
+                if((board[ibox+i][jbox+j]==board[r][c])) 
                 {
-                    this.s.setVal(i, j, M[i][j].getText().charAt(0));
-                    M[i][j].setText(String.valueOf(s.getSudoku()[i][j]));
-                    if(this.s.validInRow(i, j) 
-                       && this.s.validInColumn(i, j) 
-                       &&this.s.validInBox(i, j))
-                    {
-                        M[i][j].setBackground(Color.green);
-                    }
-                    else
-                    {
-                        M[i][j].setBackground(Color.red);
-                      
-                    }
                    
-                   
+                    return false;
                 }
+
                
             }
         }
-       
-        System.out.println(this.s);
-        System.out.println(this.s.validMatrix());
+        return true;
     }
-}
-
-
-   
+    //
+    public boolean isValidSudokuS2(char[][] board) {
+        HashSet<Integer> [] rows = new HashSet[9];
+        HashSet<Integer> [] columns = new HashSet[9];
+        HashSet<Integer> [] boxes = new HashSet[9];
+        //init sets
+        for (int i = 0; i < 9; i++) {
+          rows[i] = new HashSet<Integer>();
+          columns[i] = new HashSet<Integer>();
+          boxes[i] = new HashSet<Integer>();
+        }
     
-   
+        // validate a board
+        for (int i = 0; i < 9; i++) {
+          for (int j = 0; j < 9; j++) {
+            if (board[i][j] != '.') continue;
 
+              int n = (int)board[i][j];
+              int r=(int)(i / 3 ) * 3;
+              int c=(int)(j / 3);
+              int box_index =r + c;//0..9  
+             /*
+            rows[i].add(n):return true if  n exist in the set
+
+             */
+             if((!rows[i].add(n))
+                ||
+                (!columns[j].add(n))
+                ||
+                (!boxes[box_index].add(n))){
+                    return false;
+              }
+                
+            
+          }
+        }
+    
+        return true;
+      }
+    public String validMatrix()
+    {
+        String tmpS="{\n";
+        for(int i=0;i<9;i++)
+        {
+           
+            for(int j=0;j<9;j++)
+            {
+                String row;
+                String col;
+                String box;
+                if(this.board[i][j]=='.')
+                {
+                    tmpS+="..."+",";
+                    continue;
+                }
+                if(validInRow(i, j)) row="R";
+                else row="r";
+                if(validInColumn(i, j)) col="C";
+                else col="c";
+                if(validInBox(i, j)) box="B";
+                else {
+                    box="b";
+                    tmp+="("+i+","+j+")"+"  |";
+                } 
+                tmpS+=row+col+box+",";
+            }
+            tmpS+="\n";
+        }
+        tmpS+="}";
+        return tmpS;
+    }
+    public boolean solve(int row, int col){
+        // If completed all rows, we have solved board, return true
+        if(row==9) return true;
+        
+        // Reached to the last cell in a row, move to next row and first col
+        if(col==9) {
+            return solve(row+1,0);
+        }
+        
+        // If current cell is empty
+        if(board[row][col]=='.'){
+            for(char num='1';num<='9';num++){
+                // try placing each number at empty cell
+                // if board is still valid, move to next cell
+                boolean valid = isPossible(row,col,num);
+                
+                if(valid){
+                    // place the number
+                    board[row][col] = num;
+                    
+                    // solve for next cell
+                    boolean solved = solve(row,col+1);
+                    
+                    
+                    if(solved){
+                        return true;        
+                    }else {
+                        // if board can't be solved with current number
+                        // reset and try next number
+                        board[row][col] = '.';
+                    };
+                }
+            }
+            
+        }
+        else {
+            // if not empty, solve for next cell in same row
+            return solve(row,col+1);
+        }
+        
+        
+        return false;
+        
+    }
+    public boolean isPossible(int r,int c,char num)
+    {
+        char digit = num;
+        
+        // check if row has n
+        for(int i=0;i<9;i++){
+            if(board[r][i]==digit) return false;
+        }
+
+        // check if col has n
+        for(int i=0;i<9;i++){
+            if(board[i][c]==digit) return false;
+        }
+        
+     
+        
+        int startRowIndex = (r/3)*3;
+        int startColIndex = (c/3)*3;
+        
+        for(int i = startRowIndex; i < startRowIndex+3; i++){
+            for(int j = startColIndex; j < startColIndex+3; j++){
+                if(board[i][j]==digit) return false;
+            }
+        }
+		
+        return true;
+    }
+    @Override
+    public String toString()
+    {
+        String tmpS="{\n";
+        for(int i=0;i<9;i++)
+        {
+           
+            for(int j=0;j<9;j++)
+            {
+                tmpS+=board[i][j]+",";
+            }
+            tmpS+="\n";
+        }
+        tmpS+="}";
+        return tmpS;
+    }
+    
+}
